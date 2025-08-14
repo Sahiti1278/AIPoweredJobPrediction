98865c3f1ee915aded77a36a692d77a99e0016f2
Sahiti Sri Upputuri <98061842+Sahiti1278@users.noreply.github.com>
2025-08-14 09:25:48 -0700

    Update MonthlyCharts.py
    Updated MonthlyCharts to give each month job market analysis and its trends sorted

--- MonthlyCharts.py
+++ MonthlyCharts.py
@@ -3,92 +3,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("/Users/sahitisriupputuri/Desktop/Singular /Predictive_TrainingData.csv")
 
-# Step 1: Convert all to lowercase
+# Convert all to lowercase
 df['category_group'] = df['category_group'].str.lower()
 
-# Step 2: Merge similar categories
-def normalize_role(role):
-    if '.net' in role or 'dot net' in role or 'net' in role:
-        return '.net developer'
-    elif 'java' in role or 'developers' in role:
-        return 'java developer'
-    elif 'full' in role:
-        return 'full stack developer'
-    elif 'data engineer' in role:
-        return 'data engineer'
-    elif 'business' in role or 'business system' in role:
-        return 'business analyst'
-    elif 'devops' in role:
-        return 'devops engineer'
-    elif 'aws' in role:
-        return 'aws engineer'
-    elif 'cloud' in role:
-        return 'cloud engineer'
-    elif 'sap' in role:
-        return 'sap hana/ead consultant'
-    elif 'azure' in role:
-        return 'azure data/devops engineer'
-    elif 'software' in role:
-        return 'software development engineer'
-    elif 'python' in role:
-        return 'python developer'
-    elif 'qa' in role or 'quality' in role:
-        return 'qa engineer'
-    elif 'system engineer' in role:
-        return 'system engineer'
-    elif 'web' in role:
-        return 'web developer'
-    elif 'procurement' in role:
-        return 'procurement specialist manager'
-    elif 'sourcing' in role or 'staffing' in role or 'category' in role:
-        return 'sourcing/staffing category manager'
-    elif 'technical' in role:
-        return 'technical lead'
-    elif 'supply chain' in role:
-        return 'supply chain manager'
-    elif 'project manager' in role:
-        return 'project manager'
-    elif 'data analyst' in role or 'analytics' in role:
-        return 'data analyst'
-    elif 'angular' in role:
-        return 'angular developer'
-    elif 'front end' in role or 'front-end' in role or 'reactjs' in role or 'node' in role:
-        return 'front end developer'
-    elif 'ios' in role:
-        return 'ios developer'
-    elif 'oracle' in role:
-        return 'oracle developer'
-    elif 'architect' in role or 'etl' in role:
-        return 'data solution architect'
-    elif 'php' in role:
-        return 'php developer'
-    elif 'warehouse' in role:
-        return 'warehouse supervisor/manager'
-    elif 'snowflake' in role:
-        return 'snowflake developer'
-    elif 'salesforce' in role:
-        return 'salesforce developer'
-    elif 'logistics' in role:
-        return 'logistics coordinator/manager'
-    elif 'cyber' in role:
-        return 'cyber security analyst'
-    elif 'microsoft' in role:
-        return 'microsoft services power apps developer'
-    elif 'cobol' in role:
-        return 'cobol developer'
-    elif 'inventory' in role:
-        return 'inventory control specialist'
-    elif 'scientist' in role or 'science' in role:
-        return 'data scientist'
-    elif 'share' in role:
-        return 'share point developer'
-    elif 'sdet' in role or 'test' in role:
-        return 'software test engineer'
-    elif 'power' in role or 'bi' in role:
-        return 'power bi developer'
-    elif 'kinaxsis' in role:
-        return 'kinaxsis integration'
-    return role
 
 df['category_group'] = df['category_group'].apply(normalize_role)
 
